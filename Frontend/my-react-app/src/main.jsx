8e79768cf3017f627357306b06a55e7a7321c8d6
Hassan <hassanfaisalse@gmail.com>
2025-07-13 00:56:01 +0500

    Fixed sorting

--- Frontend/my-react-app/src/main.jsx
+++ Frontend/my-react-app/src/main.jsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { ToastProvider } from './components/ToastProvider.jsx'
 import { ConfirmProvider } from './components/ConfirmProvider.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ConfirmProvider>
       <ToastProvider>
         <App />
+        <SpeedInsights />
+        <Analytics />
       </ToastProvider>
     </ConfirmProvider>
   </StrictMode>,
